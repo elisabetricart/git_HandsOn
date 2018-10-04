1693449148459f6c19f46a12cba443cf3ab08875
Elisabet <elisabet.ricart1@uvic.cat>
2018-10-04 10:22:13 +0200

    Explicacio

--- seqClass.sh
+++ seqClass.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-seq=$1
-seq=$(echo $seq | tr a-z A-Z)  # Note we just added this line
-if [[ $seq =~ ^[ACGTU]+$ ]]; then
-  if [[ $seq =~ T ]]; then
-    echo "The sequence is DNA"
-  elif [[ $seq =~ U ]]; then
-    echo "The sequence is RNA"
-  else
-    echo "The sequence can be DNA or RNA"
-  fi
-else
-  echo "The sequence is not DNA nor RNA"
-fi
+#Canvis
+seq=$1 #Guarda sobre la variable la secuencia entrada
+seq=$(echo $seq | tr a-z A-Z) # Pasa a mayusculas los valores entrados
+if [[ $seq =~ ^[ACGTU]+$ ]]; then #Si contiene los valores dichos entrara dentro
+ if [[ $seq =~ T ]]; then #Si contiene alguna T entrara dentro
+   echo "The sequence is DNA" #Muestra el mensaje
+ elif [[ $seq =~ U ]]; then #Si contiene alguna U entrara dentro
+   echo "The sequence is RNA" #Muestra el mensaje
+ else #Si no contiene T o U entrara dentro
+  echo "The sequence can be DNA or RNA" #Muestra el mensaje
+ fi
+else #Si no contiene los valores dichos anteriormente entrara dentro
+  echo "The sequence is not DNA nor RNA" #Muestra el mensaje
+fi #Acaba el programa
+
